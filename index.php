68411edf3b040a644df8c7273046fb8a742f281c
nagli <dvd.filmas@gmail.com>
2022-07-29 14:59:04 +0300

    cleanup

--- index.php
+++ index.php
@@ -1,3 +1,33 @@
+<?php
+    //================= backend =================
+    //====base====
+    include "./models/User.php";
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $db = "vcs_web0711_db";
+    $conn = new mysqli($servername, $username, $password, $db);
+
+    //====issaugojimas====
+    if($_SERVER['REQUEST_METHOD'] =="POST"){
+        User::save($conn);
+    }
+
+    //====paemimas is duomenu bazes====
+    $conn = new mysqli($servername, $username, $password, $db);
+    $sql = "SELECT * FROM `users`";
+    $result = $conn->query($sql);
+
+    $users = [];
+        // output data of each row
+    while($row = $result->fetch_assoc()) {
+        $users[] = new User($row["id"], $row["name"], $row["surname"], $row["email"], $row["phone_number"]);
+    }
+    $conn->close();
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -19,38 +49,10 @@
         <button type="submit">išsaugoti</button>
     </form>
 
-<?php
-include "./models/User.php";
-$servername = "localhost";
-$username = "root";
-$password = "";
-$db = "vcs_web0711_db";
-$conn = new mysqli($servername, $username, $password, $db);
 
-?>
-
-<?php
-if($_SERVER['REQUEST_METHOD'] =="POST"){
-    User::save($conn);
-}
-
-
-?>
 
 
 <?php
-//================= backend =================
-
-$conn = new mysqli($servername, $username, $password, $db);
-$sql = "SELECT * FROM `users`";
-$result = $conn->query($sql);
-
-$users = [];
-    // output data of each row
-while($row = $result->fetch_assoc()) {
-    $users[] = new User($row["id"], $row["name"], $row["surname"], $row["email"], $row["phone_number"]);
-}
-  $conn->close();
 
 //================= frontend =================
 
